738cff5b25e105e1fa4f3df1d77fcde7c0180b2b
Leo Tartari <ltartari@gmail.com>
2013-03-13 00:26:34 -0300

    Varias modificações de estilo para página de perfil da empresa.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+
   def index
     @project_name_here = "Nome do Projeto Aqui"
   end
+
+  def lg
+    @project_name_here = "Nome do Projeto Aqui"
+  end
+
 end
