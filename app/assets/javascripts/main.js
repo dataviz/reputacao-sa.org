e99f6e447e4c4e8a2cb11681499dd7152dcec74c
Leo Tartari <ltartari@gmail.com>
2013-03-13 17:16:01 -0300

    Adiciona transição de cores no fundo.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,37 @@
+$(function () {
+  $(window).scroll(function () {
+    var body = $('body');
+    var currentClass = body.attr('class');
+    var newClass = newClassName();
+    if (newClass && currentClass !== newClass) {
+      body.toggleClass(currentClass + ' ' + newClass);
+    }
+    $('section').find('.section-info-wrapper').hide();
+    $('section[data-section-name=' + newClass + ']').find('.section-info-wrapper').show();
+  });
+  newClassName();
 
+  incrementCounter($('.global'));
+
+});
+
+function newClassName() {
+  var sections = $('section');
+  var windowTop = $(window).scrollTop();
+  for (var i = sections.length - 1; i >= 0; i--) {
+    var section = $(sections[i]);
+    if (windowTop >= section.offset().top) {
+      return section.data('section-name');
+    }
+  }
+}
+
+function incrementCounter(element) {
+  $self = element;
+  var counter_till = $self.data('counter');
+  var text = parseFloat($self.text());
+  $self.text(parseFloat(text)+1);
+  if ($self.text() < counter_till) {
+    setTimeout("incrementCounter($self), 40");
+  }
+}
