3fc10f8a5ba92c984acef7300ae9db74cdce4113
Leo Tartari <ltartari@gmail.com>
2013-03-15 12:47:52 -0300

    Adiciona logo, favicon e nome do projeto + algumas pequenas alterações no layout.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
+# encoding: utf-8
 class HomeController < ApplicationController
 
   def index
-    @project_name_here = "Nome do Projeto Aqui"
+    @project_name_here = "Reputação S/A"
   end
 
   def lg
-    @project_name_here = "Nome do Projeto Aqui"
+    @project_name_here = "Reputação S/A"
   end
 
 end
