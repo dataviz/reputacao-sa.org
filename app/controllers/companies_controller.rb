247c049edd3d94c229d08f5505f937180fa5e6a6
Thiago Bueno <tbueno@tbueno.com>
2013-03-15 23:10:45 +0100

    estados agora sao dinamicos

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,10 @@
+#encoding: utf-8
 class CompaniesController < ApplicationController
   def show
     @company = Company.where(strNomeFantasia: params[:nome].upcase).first
     @complaints_by_type = Hash[group(@company.complaints).sort.reverse]
+    @regions = regions
+    @states = @company.complaints_by_region
   end
 
   def group(complaints)
@@ -13,6 +16,36 @@ class CompaniesController < ApplicationController
   end
 
   def proto
+  end
+
+  def regions
+    { "AC" => ["Acre"                , "A"],
+      "AL" => ["Alagoas"             , "B"],
+      "AP" => ["Amapá"               , "C"],
+      "AM" => ["Amazonas"            , "D"],
+      "BA" => ["Bahia"               , "E"],
+      "CE" => ["Ceará"               , "F"],
+      "DF" => ["Distrito Federal"    , "G"],
+      "ES" => ["Espírito Santo"      , "H"],
+      "GO" => ["Goiás"               , "I"],
+      "MA" => ["Maranhão"            , "J"],
+      "MT" => ["Mato Grosso"         , "K"],
+      "MS" => ["Mato Grosso do Sul"  , "L"],
+      "MG" => ["Minas Gerais"        , "M"],
+      "PA" => ["Pará"                , "N"],
+      "PB" => ["Paraíba"             , "O"],
+      "PR" => ["Paraná"              , "P"],
+      "PE" => ["Pernambuco"          , "Q"],
+      "PI" => ["Piauí"               , "R"],
+      "RJ" => ["Rio de Janeiro"      , "S"],
+      "RN" => ["Rio Grande do Norte" , "T"],
+      "RS" => ["Rio Grande do Sul"   , "U"],
+      "RO" => ["Rondônia"            , "V"],
+      "RR" => ["Roraima"             , "W"],
+      "SC" => ["Santa Catarina"      , "X"],
+      "SP" => ["São Paulo"           , "Y"],
+      "SE" => ["Sergipe"             , "Z"],
+      "TO" => ["Tocantins"           , "a"]}
   end
 
 end
--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -4,8 +4,11 @@ class Complaint
   #TODO: Temporário. Apenas para extrair a lista de empresas
   field :strRazaoSocial, type: String
   field :strNomeFantasia, type: String
+  field :UF, type: String
+
   index({ strRazaoSocial: 1 }, { unique: false, name: "strRazaoSocial_index" })
   index({ strNomeFantasia: 1 }, { unique: false, name: "strNomeFantasia_index" })
+  index({ :UF => 1 }, { unique: false, name: "UF_index" })
 
   belongs_to :company
 
