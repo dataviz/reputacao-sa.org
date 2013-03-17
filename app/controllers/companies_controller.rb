2d753a62f165ac030a787e721ec07a6ce53d5db6
Thiago Bueno <tbueno@tbueno.com>
2013-03-17 23:12:57 +0100

    trocando mais hardcoded por dados reais

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,42 +2,19 @@
 class CompaniesController < ApplicationController
 
   def index
-    @companies = [
-      { name: 'OI / Brasil Telecom',         count: 15780  },
-      { name: 'Ponto Frio / Casas Bahia',         count: 15557  },
-      { name: 'Itaú / Unibanco',      count: 11079 },
-      { name: 'LG',   count: 9336 },
-      { name: 'Claro',    count: 9276  },
-      { name: 'Ricardo Eletro',        count: 7975 },
-      { name: 'Samsung', count: 7571 },
-      { name: 'Sony Ericsson',       count: 6082  },
-      { name: 'Bradesco',        count: 6758 },
-      { name: 'Nokia', count: 6074 },
-      { name: 'Tim',       count: 5959  },
-      { name: 'Vivo',  count: 5761 },
-      { name: 'Americanas.com / Submarino / Shoptime', count: 5655 },
-      { name: 'Banco do Brail',    count: 3701 },
-      { name: 'Consul / Brastemp',      count: 2874 },
-      { name: 'Citibank',       count: 2735 },
-      { name: 'Wallmart',  count: 2640   },
-      { name: 'Banco BMG',      count: 2637  },
-      { name: 'Banco Santander / Real',         count: 2606 },
-      { name: 'Mabe / GE / Dako',    count: 2544 },
-      { name: 'Carrefour',      count: 2530  }
-    ].sort_by {|company| company[:count] * -1 }
     @nav_links = nav_links
   end
 
   def show
-    #@company  = Company.where(strNomeFantasia: params[:nome].upcase).first
-
     @name = params[:name]
     @complaints = Complaint.where(strNomeFantasia: params[:name].upcase)
     @complaints_by_type = Hash[group(@complaints).sort.reverse]
     c = @complaints.first
-    @company = {nome: c.strNomeFantasia, descricao: c.DescCNAEPrincipal}
+    @company = {name: c.strNomeFantasia, descricao: c.DescCNAEPrincipal}
     @regions  = regions
     @states   = @complaints.group_by {|comp| comp.UF}
+    @solved = @complaints.select{|c| c.atendida? }.count
+    @unsolved = @complaints.reject{|c| c.atendida? }.count
 
     @nav_links = nav_links
   end
