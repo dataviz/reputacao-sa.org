4fec488be526e227cdd77b21c527979739ba5e02
Vitor Baptista <vitor@vitorbaptista.com>
2013-03-18 14:23:55 -0300

    Fecha a conexão do Mongo se sofrermos um timeout
    http://simple10.com/rescuing-from-racktimeout-to-close-mongodb-connection/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from Timeout::Error, with: :handle_timeout
+
+  def handle_timeout(exception)
+    Mongoid.database.connection.close
+  end
 end
